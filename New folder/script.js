75433e9623f40e3545ce8b6afe240f37ab45de45
Faizan Ahmad Khan <fa3n20004@gmail.com>
2024-02-11 15:27:07 +0500

    done

---
+++ New folder/script.js
@@ -0,0 +1,56 @@
+// task 1 ; sum of all digits of a number
+// let userNumber = prompt("Enter a number"); 
+
+// let sum = 0;
+
+// for( let i = 0; i < userNumber.length; i++ ) {
+
+//    sum = parseInt(userNumber[i]) + sum;
+// }
+// console.log(sum);
+
+ 
+// task 2 : sum of range of 2 numbers
+
+// let firstNum = +prompt("Enter first number");
+// let secondNum = +prompt("Enter second number");
+// let sum = 0;
+
+// for( let i = firstNum ; i <=secondNum; i++)   {
+//    sum += i;
+// }
+
+// console.log(sum);
+
+
+// task 3 average
+
+// let numbers = [12 , 13 , 15 , 18 , 19]
+// let Sum = 0;
+// let average = "";
+// for ( let i = 0; i < numbers.length; i++) {
+//     Sum = Sum + numbers[i];
+//     average = Sum / numbers.length;
+// }
+// console.log(average)
+
+// task 4 compare strings by count of characters
+
+// let stringFirst = prompt("Enter first string");
+// let stringSecond = prompt("Enter second string");
+// if(stringFirst.length == stringSecond.length)   {
+//    console.log("equal")
+// } else {
+//    console.log("not equal")
+// }
+
+
+// task 5 name -> eamn
+
+// let value = prompt("Enter a string");
+// let conainer1 = value[3];
+// let conainer2 = value[1]
+// let conainer3 = value[2];
+// let conainer4 = value[0];
+// value = conainer1 + conainer2 + conainer3 + conainer4 ;
+// console.log(value);
