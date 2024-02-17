70daa1542d21cc033553368df7c90aef56b14b97
Faizan Ahmad Khan <fa3n20004@gmail.com>
2024-02-18 00:43:16 +0500

    done with task second

---
+++ second-task/script.js
@@ -0,0 +1,35 @@
+// Task - 1 : Welcome Message
+// Create a function called displayWelcomeMessage that prompts the user to enter their name 
+// and then displays a personalized welcome message such as "Welcome, [name]!" in an alert.
+
+// function welcomeMessage() {
+//     let userName = prompt("Enter your Name :");
+//     alert("Welcome, " + userName);
+// }
+// welcomeMessage();
+
+
+// Task - 2 : Quote Generator
+// Create a function called generateQuote that randomly selects and returns a quote from an array
+//  of predefined quotes. Call this function to display a random quote on the webpage.
+
+// function generateQuote()    {
+//     let quote = ["The only way to do great work is to love what you do.",
+//     "Don’t watch the clock; do what it does. Keep going.",
+//     "Believe you can and you’re halfway there.", 
+//     "Keep calm and carry on.",
+//     "The purpose of our lives is to be happy."]
+//     let randomQuote = Math.floor(Math.random() * quote.length) ;
+//     document.write(quote[randomQuote]);
+// }
+// generateQuote();
+
+// Task - 3 : Random Number Generator
+// Write a function called generateRandomNumber that generates and returns a random number between 1 and 100. 
+// Call this function and display the generated random number on the webpage.
+
+// function generateRandomNumber() {
+//     let randomNumber = Math.ceil(Math.random() * 100) ;
+//     document.write(randomNumber)
+// }
+// generateRandomNumber();
