a2d682e1e3b98b52952c669206ccbda8b32980b3
Faizan Ahmad Khan <fa3n20004@gmail.com>
2024-02-18 00:55:30 +0500

    done update

--- 0-task/script.js
+++ 0-task/script.js
@@ -1,16 +1,15 @@
-// task 1 ; sum of all digits of a number
-// let userNumber = prompt("Enter a number"); 
+// task 1 : Sum of all digits of a number. For example, 123 -> 1+2+3=6
 
+// let userNumber = prompt("Enter a number"); 
 // let sum = 0;
 
 // for( let i = 0; i < userNumber.length; i++ ) {
-
 //    sum = parseInt(userNumber[i]) + sum;
 // }
 // console.log(sum);
 
  
-// task 2 : sum of range of 2 numbers
+// task 2 : Sum of the range of 2 numbers. For example, num1=2, num2=5 -> 2+3+4+5=14
 
 // let firstNum = +prompt("Enter first number");
 // let secondNum = +prompt("Enter second number");
@@ -19,22 +18,22 @@
 // for( let i = firstNum ; i <= secondNum; i++)   {
 //    sum += i;
 // }
-
 // console.log(sum);
 
 
-// task 3 average
+// task 3 : Calculate the average of numbers given in an array.
 
 // let numbers = [12 , 13 , 15 , 18 , 19]
 // let Sum = 0;
 // let average = "";
 // for ( let i = 0; i < numbers.length; i++) {
 //     Sum = Sum + numbers[i];
-//     average = Sum / numbers.length;
 // }
+// average = Sum / numbers.length ;
 // console.log(average)
 
-// task 4 compare strings by count of characters
+
+// task 4 : Compare strings by count of characters. For example, car and bat are equal. car and care are not.
 
 // let stringFirst = prompt("Enter first string");
 // let stringSecond = prompt("Enter second string");
@@ -45,10 +44,10 @@
 // }
 
 
-// task 5 name -> eamn
+// task 5 : Swap the string's first and last character. For example, name -> eamn
 
-let value = prompt("Enter a string");
-let swapping = value[value.length - 1] + value.slice(1, value.length -1) + value[0];
-console.log(swapping);
+// let value = prompt("Enter a string");
+// let swapping = value[value.length - 1] + value.slice(1, value.length -1) + value[0];
+// console.log(swapping);
 
 
